5925a76539cee0965b26eaac947b62aadad109d0
Yong Huang <yong.huang.sh@gmail.com>
2018-12-16 00:39:30 +0800

    Add master etcd setup scripts

---
+++ scripts/m-prep-etcd.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Run this script on masters 
+sudo rm -fR /etc/etcd /var/lib/etcd
+sudo mkdir -p /etc/etcd /var/lib/etcd
+sudo cp \
+  _certs/ca.pem _certs/ca-key.pem \
+  _certs/kube-apiserver.pem _certs/kube-apiserver-key.pem \
+  /etc/etcd
+sudo cp _scripts/out/etcd.service /etc/systemd/system/etcd.service
+sudo systemctl daemon-reload
+sudo systemctl enable etcd
+sudo systemctl start etcd
+sudo ETCDCTL_API=3 etcdctl member list \
+  --endpoints=https://127.0.0.1:2379 \
+  --cacert=/etc/etcd/ca.pem \
+  --cert=/etc/etcd/kube-apiserver.pem \
+  --key=/etc/etcd/kube-apiserver-key.pem
---
+++ scripts/m0-gen-etcd.service.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+M0_IP=172.17.59.89
+M1_IP=172.17.94.123
+M2_IP=172.17.197.159
+ETCD_NAME=m0
+INTERNAL_IP=$M0_IP
+
+cat <<EOF | tee _scripts/out/etcd.service
+[Unit]
+Description=etcd
+Documentation=https://github.com/coreos
+
+[Service]
+ExecStart=/usr/local/bin/etcd \\
+  --name ${ETCD_NAME} \\
+  --cert-file=/etc/etcd/kube-apiserver.pem \\
+  --key-file=/etc/etcd/kube-apiserver-key.pem \\
+  --peer-cert-file=/etc/etcd/kube-apiserver.pem \\
+  --peer-key-file=/etc/etcd/kube-apiserver-key.pem \\
+  --trusted-ca-file=/etc/etcd/ca.pem \\
+  --peer-trusted-ca-file=/etc/etcd/ca.pem \\
+  --peer-client-cert-auth \\
+  --client-cert-auth \\
+  --initial-advertise-peer-urls https://${INTERNAL_IP}:2380 \\
+  --listen-peer-urls https://${INTERNAL_IP}:2380 \\
+  --listen-client-urls https://${INTERNAL_IP}:2379,https://127.0.0.1:2379 \\
+  --advertise-client-urls https://${INTERNAL_IP}:2379 \\
+  --initial-cluster-token etcd-cluster-0 \\
+  --initial-cluster m0=https://${M0_IP}:2380,m1=https://${M1_IP}:2380,m2=https://${M2_IP}:2380 \\
+  --initial-cluster-state new \\
+  --data-dir=/var/lib/etcd
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
