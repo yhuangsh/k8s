864228cedf56ad63e5ad790fe06ee9e301dc3017
Yong Huang <yong.huang.sh@gmail.com>
2018-12-16 19:14:55 +0800

    Change naming convention - more files

--- scripts/gen-kubeconfig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm kubeconfig/*
-scripts/gen-kubeconfig-w0.sh
-scripts/gen-kubeconfig-kube-proxy.sh
-scripts/gen-kubeconfig-kube-controller-manager.sh
-scripts/gen-kubeconfig-kube-scheduler.sh
-scripts/gen-kubeconfig-admin.sh
-
