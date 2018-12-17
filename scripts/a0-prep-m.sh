f77ca658afbe460066b70782e6b2be6dc580c125
Yong Huang <yong.huang.sh@gmail.com>
2018-12-17 22:48:17 +0800

    Add more fixes

--- scripts/a0-prep-m.sh
+++ scripts/a0-prep-m.sh
@@ -1,53 +1,41 @@
 #!/bin/sh
 
 ssh m0 mkdir -p /home/huang/_certs
-ssh m0 mkdir -p /home/huang/_bin
 ssh m0 mkdir -p /home/huang/_kubeconfig
 ssh m0 mkdir -p /home/huang/_yaml
 ssh m0 mkdir -p /home/huang/_scripts
 
 ssh m1 mkdir -p /home/huang/_certs
-ssh m1 mkdir -p /home/huang/_bin
 ssh m1 mkdir -p /home/huang/_kubeconfig
 ssh m1 mkdir -p /home/huang/_yaml
 ssh m1 mkdir -p /home/huang/_scripts
 
 ssh m2 mkdir -p /home/huang/_certs
-ssh m2 mkdir -p /home/huang/_bin
 ssh m2 mkdir -p /home/huang/_kubeconfig
 ssh m2 mkdir -p /home/huang/_yaml
 ssh m2 mkdir -p /home/huang/_scripts
 
 scp certs/out/* m0:/home/huang/_certs
-scp bin/etcd bin/etcdctl \
-    bin/kube-apiserver bin/kube-controller-manager bin/kube-scheduler \
-    m0:/home/huang/_bin
 scp kubeconfig/admin.kubeconfig \
     kubeconfig/kube-controller-manager.kubeconfig \
     kubeconfig/kube-scheduler.kubeconfig \
     m0:/home/huang/_kubeconfig
 scp yaml/* m0:/home/huang/_yaml
-scp scripts/m* m0:/home/huang/_scripts
+scp scripts/m0* m0:/home/huang/_scripts
 
 scp certs/out/* m1:/home/huang/_certs
-scp bin/etcd bin/etcdctl \
-    bin/kube-apiserver bin/kube-controller-manager bin/kube-scheduler \
-    m1:/home/huang/_bin
 scp kubeconfig/admin.kubeconfig \
     kubeconfig/kube-controller-manager.kubeconfig \
     kubeconfig/kube-scheduler.kubeconfig \
     m1:/home/huang/_kubeconfig
 scp yaml/* m1:/home/huang/_yaml
-scp scripts/m* m1:/home/huang/_scripts
+scp scripts/m1* m1:/home/huang/_scripts
 
 scp certs/out/* m2:/home/huang/_certs
-scp bin/etcd bin/etcdctl \
-    bin/kube-apiserver bin/kube-controller-manager bin/kube-scheduler \
-    m2:/home/huang/_bin
 scp kubeconfig/admin.kubeconfig \
     kubeconfig/kube-controller-manager.kubeconfig \
     kubeconfig/kube-scheduler.kubeconfig \
     m2:/home/huang/_kubeconfig
 scp yaml/* m2:/home/huang/_yaml
-scp scripts/m* m2:/home/huang/_scripts
+scp scripts/m2* m2:/home/huang/_scripts
 
