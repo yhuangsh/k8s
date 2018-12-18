9b84925fd38b748e7a0efe8bde5fe12662d8395a
Yong Huang <yong.huang.sh@gmail.com>
2018-12-18 08:37:57 +0800

    Change curl command to avoid having extra http header to the binary

--- scripts/_dl-bin-vpn.sh
+++ scripts/_dl-bin-vpn.sh
@@ -5,9 +5,9 @@ mkdir -p bin/download
 # Download Kubernetes binaries, Start VPN when in China
 KUBE_VER=v1.13.0
 URL=https://storage.googleapis.com/kubernetes-release/release/${KUBE_VER}/bin/linux/amd64
-curl -i -L ${URL}/kube-apiserver -o bin/kube-apiserver
-curl -i -L ${URL}/kube-controller-manager -o bin/kube-controller-manager
-curl -i -L ${URL}/kube-scheduler -o bin/kube-scheduler
-curl -i -L ${URL}/kubectl -o bin/kubectl
+curl ${URL}/kube-apiserver > bin/kube-apiserver
+curl ${URL}/kube-controller-manager > bin/kube-controller-manager
+curl ${URL}/kube-scheduler > bin/kube-scheduler
+curl ${URL}/kubectl > bin/kubectl
 
 chmod +x bin/kube-apiserver bin/kube-controller-manager bin/kube-scheduler bin/kubectl
