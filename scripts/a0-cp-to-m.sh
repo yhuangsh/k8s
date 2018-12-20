ac3f19190a421c4e7a957f95ef9229be3c6712a0
Yong Huang <yong.huang.sh@gmail.com>
2018-12-21 00:32:14 +0800

    Add copying of yaml to masters

--- scripts/a0-cp-to-m.sh
+++ scripts/a0-cp-to-m.sh
@@ -2,27 +2,33 @@
 
 ssh m0 mkdir -p /home/huang/_certs
 ssh m0 mkdir -p /home/huang/_scripts
+ssh m0 mkdir -p /home/huang/_yaml
 
 ssh m1 mkdir -p /home/huang/_certs
 ssh m1 mkdir -p /home/huang/_scripts
+ssh m1 mkdir -p /home/huang/_yaml
 
 ssh m2 mkdir -p /home/huang/_certs
 ssh m2 mkdir -p /home/huang/_scripts
+ssh m2 mkdir -p /home/huang/_yaml
 
 echo "Copying certs and scripts files to m0"
 scp certs/out/* m0:/home/huang/_certs
+scp yaml/* m0:/home/huang/_yaml
 scp scripts/m-* m0:/home/huang/_scripts
 scp scripts/m0* m0:/home/huang/_scripts
 ssh m0 chmod +x _scripts/*
 
 echo "Copying certs and scripts files to m1"
 scp certs/out/* m1:/home/huang/_certs
+scp yaml/* m1:/home/huang/_yaml
 scp scripts/m-* m1:/home/huang/_scripts
 scp scripts/m1* m1:/home/huang/_scripts
 ssh m1 chmod +x _scripts/*
 
 echo "Copying certs and scripts files to m2"
 scp certs/out/* m2:/home/huang/_certs
+scp yaml/* m2:/home/huang/_yaml
 scp scripts/m-* m2:/home/huang/_scripts
 scp scripts/m2* m2:/home/huang/_scripts
 ssh m2 chmod +x _scripts/*
