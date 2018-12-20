ee5b4c75f488efef05321b5ac052ebd0e9d54226
Yong Huang <yong.huang.sh@gmail.com>
2018-12-20 23:46:43 +0800

    Add sudo to load image

--- scripts/m-load-kubeadm-images.sh
+++ scripts/m-load-kubeadm-images.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 KUBE_VER=v1.13.1
-docker load < _bin/kube-apiserver_${KUBE_VER}
-docker load < _bin/kube-controller-manager_${KUBE_VER}
-docker load < _bin/kube-scheduler_${KUBE_VER}
-docker load < _bin/kube-proxy_${KUBE_VER}
-docker load < _bin/pause_3.1
-docker load < _bin/etcd_3.2.24
-docker load < _bin/coredns_1.2.6
+sudo docker load < _bin/kube-apiserver_${KUBE_VER}
+sudo docker load < _bin/kube-controller-manager_${KUBE_VER}
+sudo docker load < _bin/kube-scheduler_${KUBE_VER}
+sudo docker load < _bin/kube-proxy_${KUBE_VER}
+sudo docker load < _bin/pause_3.1
+sudo docker load < _bin/etcd_3.2.24
+sudo docker load < _bin/coredns_1.2.6
