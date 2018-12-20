5110e08bd186d442956ac2f59d716e65c4a30152
Yong Huang <yong.huang.sh@gmail.com>
2018-12-20 23:55:56 +0800

    Add script to generate kubeadm-config.yaml

---
+++ scripts/m-gen-kubeadm-config.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+A0_IP=172.17.94.124
+M0_IP=172.17.59.89
+M1_IP=172.17.94.123
+M2_IP=172.17.197.159
+
+mkdir -p /home/huang/_yaml
+cat <<EOF | tee _yaml/kubeadm-config.yaml
+apiVersion: kubeadm.k8s.io/v1alpha3
+kind: ClusterConfiguration
+kubernetesVersion: stable
+apiServerCertSANs:
+- ${A0_IP}
+controlPlaneEndpoint: "${A0_IP}:6443"
+etcd:
+  external:
+    endpoints:
+    - https://${M0_IP}:2379
+    - https://${M1_IP}:2379
+    - https://${M2_IP}:2379
+    caFile: /etc/etcd/ca.pem
+    certFile: /etc/etcd/kubernetes.pem
+    keyFile: /etc/etcd/kubernetes-key.pem
+networking:
+  podSubnet: 10.30.0.0/24
+apiServerExtraArgs:
+  apiserver-count: "3"
+EOF
