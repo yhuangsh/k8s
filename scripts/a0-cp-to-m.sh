ebff5df22f9d5ae9951ad3276b9c392b888022ac
Yong Huang <yong.huang.sh@gmail.com>
2018-12-20 23:29:49 +0800

    Make scripts executable after copying

--- scripts/a0-cp-to-m.sh
+++ scripts/a0-cp-to-m.sh
@@ -13,14 +13,16 @@ echo "Copying certs and scripts files to m0"
 scp certs/out/* m0:/home/huang/_certs
 scp scripts/m-* m0:/home/huang/_scripts
 scp scripts/m0* m0:/home/huang/_scripts
+ssh m0 chmod +x _scripts/*
 
 echo "Copying certs and scripts files to m1"
 scp certs/out/* m1:/home/huang/_certs
 scp scripts/m-* m1:/home/huang/_scripts
 scp scripts/m1* m1:/home/huang/_scripts
+ssh m1 chmod +x _scripts/*
 
 echo "Copying certs and scripts files to m2"
 scp certs/out/* m2:/home/huang/_certs
 scp scripts/m-* m2:/home/huang/_scripts
 scp scripts/m2* m2:/home/huang/_scripts
-
+ssh m2 chmod +x _scripts/*
