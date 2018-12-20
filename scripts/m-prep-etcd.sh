62d63f61cab8cad0fb15eda7a7abb1747d16a505
Yong Huang <yong.huang.sh@gmail.com>
2018-12-20 23:27:15 +0800

    Remove member list

--- scripts/m-prep-etcd.sh
+++ scripts/m-prep-etcd.sh
@@ -10,8 +10,3 @@ sudo cp _scripts/out/etcd.service /etc/systemd/system/etcd.service
 sudo systemctl daemon-reload
 sudo systemctl enable etcd
 sudo systemctl start etcd
-sudo ETCDCTL_API=3 etcdctl member list \
-  --endpoints=https://127.0.0.1:2379 \
-  --cacert=/etc/etcd/ca.pem \
-  --cert=/etc/etcd/kubernetes.pem \
-  --key=/etc/etcd/kubernetes-key.pem
